68eabe7492664abe23fc7bcc68ec6a8cae8f884b
HARI PRASATH N T <87246155+zogratis17@users.noreply.github.com>
2025-03-26 16:24:44 +0530

    Introduction Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);  // Removed "initialState:"
+
+  useEffect(() => {
+    console.log(`${title} has been liked : ${count} times`);
+  });
+
+  // When Strict Mode is on, in development , React runs setup and cleanup one extra time before the actual setup. Therefore , the effect runs twice when the component mounts
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='card' onClick = {() => setCount(count+1)}>
+      <h2>{title} <br /> {count} </h2>
+      <button onClick={() => setHasLiked(!hasLiked)}>  {/* Toggle like state */}
+        {hasLiked ? '❤️' : '🤍'}
+      </button>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className='card-container'>
+      <Card title="Game of Thrones" />
+      <Card title="Harry Potter" />
+      <Card title="Solo Leveling" />
+    </div>
+  );
 }
 
-export default App
+export default App;
